b9ad388c0d42e6f913c89183a4db8ad76489ee14
Darthmemes <98658478+Darthmemes@users.noreply.github.com>
2022-05-10 16:58:24 -0500

    s

--- SchedulingApp/src/app/app.module.ts
+++ SchedulingApp/src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalendarComponent } from './calendar/calendar.component';
+import { DateDetailsComponent } from './date-details/date-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    DateDetailsComponent
   ],
   imports: [
     BrowserModule,
